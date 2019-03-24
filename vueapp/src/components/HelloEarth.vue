57bea6d5e92138002cef854706b3e24744e51f4c
Codependency <835770118@qq.com>
2019-03-24 20:53:17 +0800

    first commit

---
+++ vueapp/src/components/HelloEarth.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>HEllo Earth</h2>
+    <h2>{{this.$route.params.earthmsg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloEarth',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vueapp/src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import router from 'vue-router'
+import HelloWorld from '../components/HelloWorld'
+import HelloEarth from '../components/HelloEarth'
+import list from '../components/list'
+Vue.use(router)
+
+// 模块的导出
+/* eslint-disable no-new */
+export default new router({
+    routes: [{
+        name:'helloworld',
+        path: '/helloworld/?worldmsg',
+        component: HelloWorld
+      },{
+        name:'helloearth',
+        path: '/helloearth/:earthmsg',
+        component: HelloEarth
+      },{
+        path: '/list',
+        component: list
+      }
+    ]
+})
