c2499952057090e5145b80619652b44894022c52
Codependency <835770118@qq.com>
2019-03-27 13:04:35 +0800

    demo

--- vuexapp/src/router/index.js
+++ vuexapp/src/router/index.js
@@ -2,16 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
-import Vuex from 'vuex'
 
-
-Vue.use(Vuex)
-
-var store = new Vuex.Store({
-  state:{
-    num: 100
-  }
-})
 
 Vue.use(Router)
 export default new Router({
